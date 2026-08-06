865c80cc8db8aa99ad8341238a4c1fc6e559de94
6oJIb <xmizor228@gmail.com>
2026-08-07 02:05:49 +0300

    added set_voice_creator command

--- bot.py
+++ bot.py
@@ -1,22 +1,15 @@
 import asyncio
-from unicodedata import category
-from dotenv import load_dotenv
 import os
 import disnake
 from disnake.ext import commands
 
-bot = commands.Bot(command_prefix="/", intents=disnake.Intents.all())
-
+bot = commands.Bot(command_prefix="/", intents=disnake.Intents.all(), test_guilds=[1534901683690274916, 1385567845089542204])
+activeVoices = []
 voiceStatuses = {
-    1534927334371754116: "<:default1:1535021935430209536>" + "<:default2:1535021950181707866>" + "<:default3:1535021962970136768>" + "<:default4:1535021971115475014>",
-    1535025690704089161: "<:vip1:1535024198278447206>" + "<:vip2:1535024308106166332>",
-    1535025706277273702: "<:admin1:1535024872512688288>" + "<:admin2:1535024888975327322>" + "<:admin3:1535024899184267394>" + "<:admin4:1535024909326221724>"
+    1534927334371754116: ["default", "<:default1:1535021935430209536>" + "<:default2:1535021950181707866>" + "<:default3:1535021962970136768>" + "<:default4:1535021971115475014>"],
+    1535025690704089161: ["vip", "<:vip1:1535024198278447206>" + "<:vip2:1535024308106166332>"],
+    1535025706277273702: ["admin", "<:admin1:1535024872512688288>" + "<:admin2:1535024888975327322>" + "<:admin3:1535024899184267394>" + "<:admin4:1535024909326221724>"]
 }
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name} ({bot.user.id})")
-    print("--------------------------------------------------")
 
 # class VoiceControlPanel(disnake.ui.View):
 #     def __init__(self):
@@ -43,7 +36,23 @@ async def on_ready():
             # embed.set_footer(text="wasixs bot")
             # await new_channel.send(embed=embed, view=VoiceControlPanel())
 
-activeVoices = []
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name} ({bot.user.id})")
+    print("--------------------------------------------------")
+
+@bot.slash_command()
+async def set_voice_creator(inter, channel: disnake.VoiceChannel, mode: str):
+    for key, value in voiceStatuses.items():
+        mode_name, emoji_string = value
+        if mode_name == mode:
+            voiceStatuses[channel.id] = voiceStatuses.pop(key)
+
+            await inter.response.send_message(
+                f"[{mode}] Voice creator set in [{channel.mention}]!", ephemeral=True)
+            return
+
+    await inter.response.send_message(f"Mode [{mode}] not found!", ephemeral=True)
 
 @bot.event
 async def on_voice_state_update(member: disnake.Member, before, after: disnake.VoiceState):
@@ -51,7 +60,10 @@ async def on_voice_state_update(member: disnake.Member, before, after: disnake.V
         if before.channel.id in activeVoices:
             if len(before.channel.members) == 0:
                 activeVoices.remove(before.channel.id)
-                await before.channel.delete()
+                try:
+                    await before.channel.delete()
+                except disnake.NotFound:
+                    pass
 
     if after.channel is not None:
         id: int = after.channel.id
@@ -68,19 +80,8 @@ async def on_voice_state_update(member: disnake.Member, before, after: disnake.V
 
             next_number = max(numbers) + 1 if numbers else 1
 
-            new_channel = await after.channel.clone(
-                name=f"voice {next_number}",
-                user_limit=99,
-                position=len(category.channels)
-            )
+            new_channel = await after.channel.clone(name=f"voice {next_number}", user_limit=99, position=len(category.channels))
             activeVoices.append(new_channel.id)
-
-            route = disnake.http.Route(
-                "PUT",
-                "/channels/{channel_id}/voice-status",
-                channel_id=new_channel.id
-            )
-            await bot.http.request(route, json={"status": voiceStatuses[id]})
 
             await asyncio.sleep(0.1)
             try:
@@ -89,12 +90,13 @@ async def on_voice_state_update(member: disnake.Member, before, after: disnake.V
                 await asyncio.sleep(0.1)
                 await member.move_to(new_channel)
 
-            
-        
-            
-        
+            route = disnake.http.Route("PUT", "/channels/{channel_id}/voice-status", channel_id=new_channel.id)
+            await bot.http.request(route, json={"status": voiceStatuses[id][1]})
 
+@bot.event
+async def on_guild_channel_delete(channel):
+    if channel.id in activeVoices:
+        activeVoices.remove(channel.id)         
 
-load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 bot.run(TOKEN)
